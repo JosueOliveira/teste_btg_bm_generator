5ffdc6aed71a5221464eff9b5a99bab1ed6e182a
Josue Oliveira <josueoloveira29@gmail.com>
2025-08-07 16:43:14 -0300

    Incluido rotinas de testes

---
+++ BMGenerate_Test/BMGeneratorServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using BMGeneratorTest.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMGenerate_Test;
+public class BMGeneratorServiceTests
+{
+    private readonly BMGeneratorService bMGeneratorService;
+
+    public BMGeneratorServiceTests()
+    {
+        bMGeneratorService = new BMGeneratorService();
+    }
+
+    [Fact]
+    public void GenerateBrowniaMotionTest()
+    {
+        var values = bMGeneratorService.GenerateBrowniaMotion(sigma:10, mean:20, initialPrice:100, numDays: 150);
+
+        Assert.Equal(150, values.Count);
+    }
+}
